366f6f7a623b09f312d9b86e7782880c42959898
Mutinda-mj <tmutinda44@gmail.com>
2025-03-25 10:18:36 +0300

    Update py_assgno.py

---
+++ py_assgno.py
@@ -0,0 +1,13 @@
+my_list=[]
+# values=list(range(10,31,10))
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.insert(1,15)
+list_B=[50,60,70]
+my_list.extend(list_B)
+my_list.pop()
+# my_list.sort()
+print(my_list.index(30))
+print(my_list[3])
+print(my_list)
